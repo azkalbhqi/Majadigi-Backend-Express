29455046874380fa9711855b2e0cd6729de455a0
azkalbhqi <azkalpredator@gmail.com>
2026-05-27 20:44:29 +0700

    revert: debug controller logs

--- src/features/opendata/opendata.controller.js
+++ src/features/opendata/opendata.controller.js
@@ -1,6 +1,4 @@
 import * as OpenDataService from './opendata.service.js';
-import fs from 'fs';
-import path from 'path';
 
 export const listDatasets = async (req, res, next) => {
   try {
@@ -12,12 +10,6 @@ export const listDatasets = async (req, res, next) => {
       message: data.length > 0 ? 'Datasets found' : 'No datasets found',
       pagination: {
         total: data.length
-      },
-      debug: {
-        cwd: process.cwd(),
-        dirContents: fs.existsSync(process.cwd()) ? fs.readdirSync(process.cwd()) : [],
-        fallbackExists: fs.existsSync(path.join(process.cwd(), 'data-fallback')),
-        fallbackDirContents: fs.existsSync(path.join(process.cwd(), 'data-fallback')) ? fs.readdirSync(path.join(process.cwd(), 'data-fallback')) : []
       },
       data
     });
